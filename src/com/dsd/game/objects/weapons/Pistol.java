705654d8e097a6cf84aafc7673139a8b1eef3a80
Rinty <r_chowdh@uncg.edu>
2019-10-14 14:11:13 -0400

    updated file name

--- src/com/dsd/game/objects/weapons/Pistol.java
+++ src/com/dsd/game/objects/weapons/Pistol.java
@@ -32,9 +32,9 @@ public class Pistol extends Gun {
         super(WeaponType.PISTOL, 16, _game, _player, _sch);
         //  Instantiates the animation controllers
         StandardAnimatorController walkingAnimation = new StandardAnimatorController(
-                new StandardAnimation(_player, Utilities.loadFrames("src/resources/img/player/player_walk_gun/", 6), WALKING_FPS));
+                new StandardAnimation(_player, Utilities.loadFrames("src/resources/img/player/player_male/player_walk_gun/", 6), WALKING_FPS));
         StandardAnimatorController shootingAnimation = new StandardAnimatorController(
-                new StandardAnimation(_player, Utilities.loadFrames("src/resources/img/player/player_shoot_gun/", 5), SHOOT_GUN_FPS));
+                new StandardAnimation(_player, Utilities.loadFrames("src/resources/img/player/player_male/player_shoot_gun/", 5), SHOOT_GUN_FPS));
         super.setWalkFrames(walkingAnimation);
         super.setAttackFrames(shootingAnimation);
         super.setDelay(this.DELAY);
